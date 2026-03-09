f5fdeb4e8c7d9f8ed446182f2cb1f4209e18a2cc
navjotschahal <navjotsinghchahal@outlook.com>
2026-03-09 16:21:18 -0400

    fix: restore original navbar DOM structure for CSS compatibility

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import aboutData from '../data/about.json';
 import './Navbar.css';
 
 const NavbarComponent: React.FC = () => {
   return (
     <nav className="navbar">
-      {/* Your existing Navbar links remain the same */}
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/experience">Experience</Link>
-        <Link to="/research">Research</Link>
-        <Link to="/resume">Resume</Link>
+      <div className="navbar-brand">
+        <NavLink to="/">{aboutData.name.split(' ').map((w: string) => w[0]).join('')}</NavLink>
       </div>
+      <ul className="navbar-links">
+        <li><NavLink to="/" end>Home</NavLink></li>
+        <li><NavLink to="/research">Research</NavLink></li>
+        <li><NavLink to="/projects">Projects</NavLink></li>
+        <li><NavLink to="/experience">Experience</NavLink></li>
+        <li><NavLink to="/resume">Resume</NavLink></li>
+      </ul>
     </nav>
   );
-};
+}
 
 export default NavbarComponent;
